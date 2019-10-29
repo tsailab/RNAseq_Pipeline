de90106e5194b9ee117da4baad0a3da7d9bc6ae6
Jacob Allen Reeves <reeves17@sapelo2-sub1.ecompute>
2019-10-29 16:17:19 -0400

    initial stuff for build.sh

---
+++ build.sh
@@ -0,0 +1,29 @@
+###
+### Directories
+###
+
+rootDir=$1
+fastqDir=${rootDir}/fastq              # project root
+cleanDir=${rootDir}/clean              # fastq files go here
+mapDir=${rootDir}/map                  # mapping output
+resultsDir=${rootDir}/resultsForPt2    # results used for part 2 are copied here
+NGScleanDir=${rootDir}/NGSclean        # NGSclean git repo gets cloned here
+scriptsDir=${rootDir}/scripts             # scripts get written here
+
+
+###
+### Make directories
+###
+
+mkdir -p $rootDir
+mkdir $fastqDir
+mkdir $cleanDir
+mkdir $mapDir
+mkdir $resultsDir
+mkdir $scriptsDir
+
+
+###
+### Clone NGS clean dir
+###
+git clone https://github.com/tsailab/NGSclean.git $NGScleanDir
