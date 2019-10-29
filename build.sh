2517e77bc2716e83d16130296a2573923455f478
Jacob Allen Reeves <reeves17@sapelo2-sub1.ecompute>
2019-10-29 17:35:39 -0400

    finished parameter checking

--- build.sh
+++ build.sh
@@ -1,8 +1,46 @@
 ###
-### Usage:
-### This script will create all directories, scripts, and 
-### $1 root directory for 
+### This script will create all directories and scripts used in part 1 of the
+### RNAseq analysis
+###
+### USAGE:
+###  $1  root directory for project
+###  $2  Paired end single end reads
+###        "pe" for paired-end reads
+###        "se" for single-end reads
+###
 
+
+###
+### Parameter check
+###
+
+if [ ! -z "$1" ]
+then
+    rootDir=$1
+    echo $rootDir
+else
+    echo "You have not entered the correct number of parameters"
+    exit 0
+fi
+
+
+if [ ! -z "$2" ]
+then
+    if [ "$2" = "pe" ]
+    then
+        isPaired=1
+    elif [ "$2" = "se" ]
+    then
+        isPaired=0
+    else
+        echo "paired vs single end read param incorrect format."
+        echo "Use either 'pe' for paired-end reads or 'se' for single-end reads"
+        exit 0
+    fi
+else
+    echo "You have not entered the correct number of parameters"
+    exit 0
+fi
 
 
 ###
@@ -14,7 +52,7 @@ cleanDir=${rootDir}/clean              # fastq files go here
 mapDir=${rootDir}/map                  # mapping output
 resultsDir=${rootDir}/resultsForPt2    # results used for part 2 are copied here
 NGScleanDir=${rootDir}/NGSclean        # NGSclean git repo gets cloned here
-scriptsDir=${rootDir}/scripts             # scripts get written here
+scriptsDir=${rootDir}/scripts          # scripts get written here
 
 
 ###
@@ -33,18 +71,3 @@ mkdir $scriptsDir
 ###
 git clone https://github.com/tsailab/NGSclean.git $NGScleanDir
 
-
-###
-### 
-
-alskjdflaksdf
-asdfjka;skljdf
-asdfkljadf
-
-
-
-
-
-
-
-as;lkdfh;alskjfd;lkasdf
