ae0ad221e6d6b03f10bee7e67b364c4cc98d493b
Jacob Allen Reeves <reeves17@xfer3.gacrc.uga.edu>
2019-11-01 16:29:36 -0400

    finished my part ~Jake

--- build.sh
+++ build.sh
@@ -24,7 +24,6 @@
 if [ ! -z "$1" ]
 then
     rootDir=$1
-    echo $rootDir
 else
     echo "   You have not entered the correct number of parameters"
     echo "   Refer to the instructions in the README.md file for more information."
@@ -91,6 +90,7 @@ rootDir=$1
 fastqDir=${rootDir}/fastq              # project root
 cleanDir=${rootDir}/clean              # fastq files go here
 mapDir=${rootDir}/map                  # mapping output
+countDir=${rootDir}/count
 resultsDir=${rootDir}/resultsForPt2    # results used for part 2 are copied here
 NGScleanDir=${rootDir}/NGSclean        # NGSclean git repo gets cloned here
 scriptsDir=${rootDir}/scripts          # scripts get written here
@@ -104,6 +104,7 @@ mkdir -p $rootDir
 mkdir $fastqDir
 mkdir $cleanDir
 mkdir $mapDir
+mkdir $countDir
 mkdir $resultsDir
 mkdir $scriptsDir
 
@@ -111,14 +112,15 @@ mkdir $scriptsDir
 ###
 ### Clone NGS clean dir
 ###
-#git clone https://github.com/tsailab/NGSclean.git $NGScleanDir
+git clone https://github.com/tsailab/NGSclean.git $NGScleanDir
 
 
 ###
 ### Copy genome and annotation
 ###
-#cp -r ${genomeOriDir} ${genomeDir}
-#cp ${genomeAnnoDir}/*.gtf ${genomeDir}
+cp -r ${genomeOriDir} ${genomeDir}
+gtfFile=${genomeAnnoDir}/$(ls $genomeAnnoDir | grep 'gtf$' | head -1)
+cp ${gtfFile} ${genomeDir}/gene.gtf
 
 
 ###
@@ -145,7 +147,7 @@ workingDir=${mapDir}
 dataDir=${cleanDir}
 genome=${genomeDir}
 
-printf "cd $workingDir\n">>$mapScript
+printf "cd $workingDir\n">$mapScript
 printf "master=\"Shell_master_STAR.sh\"\n">>$mapScript
 printf "printf \"#\"\'!\'/bin/bash\"\\\n\">\$master\n">>$mapScript
 printf "index=0\n">>$mapScript
@@ -194,3 +196,58 @@ printf "## submit shell script\n">>$mapScript
 printf "chmod 770 Shell_master_STAR.sh\n">>$mapScript
 printf "bash Shell_master_STAR.sh\n">>$mapScript
 printf "\n">>$mapScript
+
+
+###
+### Read Counting
+###
+countScript=${scriptsDir}/05count.sh
+workingDir=${countDir}
+bamDir=${mapDir}
+
+printf "cd $workingDir\n">$countScript
+printf "\n">>$countScript
+printf "master=\"Shell_master_FeatureC.sh\"\n">>$countScript
+printf "printf \"#\"\'!\'/bin/bash\"\\\n\" >\$master\n">>$countScript
+printf "index=0\n">>$countScript
+printf "for bam in \$bamDir/*.bam\n">>$countScript
+printf "do\n">>$countScript
+printf "    sample1=\${bam%%%%Aligned.sortedByCoord.out.bam}\n">>$countScript
+printf "    sample=\${sample1##*/}\n">>$countScript
+printf "    out=\$sample\"_counts.txt\"\n">>$countScript
+printf "    index=\$((\$index+1))\n">>$countScript
+printf "    sh_worker=\"run\"\$index\"_\"\$sample\".sh\"\n">>$countScript
+printf "    printf \"qsub \"\$sh_worker\"\\\n\">>\$master\n">>$countScript
+printf "    printf \"#\"\'!\'/bin/bash\"\\\n\">\$sh_worker\n">>$countScript
+printf "    printf \"#PBS -N FeatureC_\"\$sample\"\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \"#PBS -q batch\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \"#PBS -l nodes=1:ppn=12:Intel\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \"#PBS -l walltime=12:00:00\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \"#PBS -l mem=30gb\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \"cd $workingDir\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \"ml Subread/1.6.2 \\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \'featureCounts -Q 2 -M --fraction -s 0 -T 12 -p -C \\\\\\\'\"\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \' -a $genomeDir/gene.gtf \\\\\\\'\"\\\n\">>\$sh_worker\n">>$countScript
+printf "    printf \' -o \'\$out\' \'\$bam\' \'>>\$sh_worker\n">>$countScript
+printf "done\n">>$countScript
+printf "\n">>$countScript
+printf "## submit jobs\n">>$countScript
+printf "chmod 770 \$master\n">>$countScript
+printf "bash \$master\n">>$countScript
+
+
+###
+### Count merge
+###
+mergeScript=${scriptsDir}/06merge.sh
+printf "cd $countDir\n">$mergeScript
+printf "ml Python/3.6.6-foss-2018b\n">>$mergeScript
+printf "python $NGScleanDir/Merge_featureCounts_Folder.py $countDir all_counts.tsv\n">>$mergeScript
+
+
+###
+### Put results for step 2 in $resultsDir
+###
+resultsScript=${scriptsDir}/07get-results.sh
+printf "grep \"\" $mapDir/*Log.final.out > $resultsDir/all_mapping_logs.txt\n">$resultsScript
+printf "cp $countDir/all_counts.tsv $resultsDir/counts.txt\n">>$resultsScript
